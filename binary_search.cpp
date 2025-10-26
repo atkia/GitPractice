7307521c311c7c6432b5f722b8bd62f9fea94fd5
Atkia <bsse1015@iit.du.ac.bd>
2025-10-26 09:27:01 +0600

    First Commit

---
+++ binary_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a[9] = {1,2,3,4,5,6,7,8,9};
+
+    int n;
+    cin>>n;
+
+    int low = 0;
+    int high = 8;
+
+    bool found = false;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(a[mid]==n){
+            cout<<"Elements found"<<endl;
+            found = true;
+            break;
+        }
+        else if(a[mid]<n){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    if(!found){
+        cout<<"Elements not found"<<endl;
+    }
+}
